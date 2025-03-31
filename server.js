4bb9959a41cf5177f6b2ebe45c3e84c1828cef4c
purnesh607 <68152494+purnesh607@users.noreply.github.com>
2025-03-31 14:58:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.post("/contact", async (req, res) => {
 
 // Serve index.html for root route
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "public", "Index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
